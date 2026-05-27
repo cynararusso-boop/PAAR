7e4babe4a9735c80c99e584cf049267d4748416a
Seu Nome <seuemail@exemplo.com>
2026-05-27 17:15:53 -0300

    corrige envio Google Sheets

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -1,6 +1,6 @@
 export async function salvarTriagem(dados: any) {
   try {
-    const resposta = await fetch("https://script.google.com/macros/s/AKfycbyH6rsXEACYSbALhy0GQqYmRtSf4y5XdWUSuLBpJ7ZY_KeWj3zNaHBXtVRUOT6g1GkG/exec", {
+    const resposta = await fetch("https://script.google.com/macros/s/AKfycbwByIQm3OpWQvd8RHeA7Su9J7cwHCo1OrpttAje2yePYUly8sspBPS9MeuDMjrqvVre/exec", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
